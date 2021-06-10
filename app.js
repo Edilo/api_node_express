41e8a2e4883501f7aad71d1aee37c15079956cee
Edilo <edilo13@gmail.com>
2021-06-10 16:06:34 -0400

    Atualização no jwt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors');
-const {promisify} = require('util');// ferramenta de callback node.
+const { eAdmin } = require('./middlewares/auth');
 
 app.use(express.json());
 
@@ -19,7 +19,7 @@ app.get('/', function(req, res){
     return res.send('Api rest com express');
 });
 
-app.get('/usuarios', validarToken, function(req, res){
+app.get('/usuarios', eAdmin, function(req, res){
     return res.json({
         erro: false,
         message: "Listar usuários"
@@ -49,42 +49,7 @@ app.post('/login', function(req, res){
     
 });
 
-async function validarToken(req, res, next){
-    // return res.json({message: "Validar o token!"});
-    const authHeader = req.headers.authorization;
-    
-    if(!authHeader){
-        return res.json({
-            erro: true,
-            message: "Erro: Necessário realizar o login para acessar a página!"
-        });
-    }
-    
 
-    try{
-        const [, token] = authHeader.split(' ');
-        const decode = await promisify(jwt.verify)(token, process.env.SECRET);
-        req.userId = decode.id;
-        return next();
-    }catch(err){
-        return res.json({
-            erro: true,
-            message: "Erro: Login ou senha inválido!"
-        });
-    }
-    
-        // const [, token] = authHeader.split(' ');
-        // return res.json({token});
-    
-    //const [, token] = authHeader.split(' ');
-    //return res.json({token});
-    // if(!token){
-    //     return res.json({
-    //         erro: true,
-    //         message: "Erro: Token inválido!"
-    //     });
-    // }
-}
 
 app.listen(8080, function(){
     console.log('Servidor iniciado na porta 8080, http://localhost:8080');
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,42 @@
 const jwt = require('jsonwebtoken');
-const {promisify} = require('uti');
+const {promisify} = require('util');// ferramenta de callback node.
 require('dotenv').config();
+
+module.exports = {
+    eAdmin: async function (req, res, next){
+        // return res.json({message: "Validar o token!"});
+        const authHeader = req.headers.authorization;
+        
+        if(!authHeader){
+            return res.json({
+                erro: true,
+                message: "Erro: Necessário realizar o login para acessar a página!"
+            });
+        }
+        
+    
+        try{
+            const [, token] = authHeader.split(' ');
+            const decode = await promisify(jwt.verify)(token, process.env.SECRET);
+            req.userId = decode.id;
+            return next();
+        }catch(err){
+            return res.json({
+                erro: true,
+                message: "Erro: Login ou senha inválido!"
+            });
+        }
+        
+            // const [, token] = authHeader.split(' ');
+            // return res.json({token});
+        
+        //const [, token] = authHeader.split(' ');
+        //return res.json({token});
+        // if(!token){
+        //     return res.json({
+        //         erro: true,
+        //         message: "Erro: Token inválido!"
+        //     });
+        // }
+    }
+}
